868f3ef65510fb7b3451d21b7d7bb7f645d69958
BradwellC <59516796+BradwellC@users.noreply.github.com>
2022-04-25 15:55:02 +0100

    v1

--- public/js/base.js
+++ public/js/base.js
@@ -89,3 +89,17 @@ $(document).ready(function () {
         } // End if
     });
 });
+
+const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
+
+portfolioItems.forEach(portfolioItem => {
+    portfolioItem.addEventListener('mouseover', () => {
+        console.log(portfolioItem.childNodes[1].classList)
+        portfolioItem.childNodes[1].classList.add('image-blur');
+    });
+
+    portfolioItem.addEventListener('mouseout', () => {
+        console.log(portfolioItem.childNodes[1].classList)
+        portfolioItem.childNodes[1].classList.remove('image-blur');
+    });
+});
